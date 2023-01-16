85ab38672446f1e26540cd59980fc1591914fbb8
Alberlando Herculano <alberlandoherculano@gmail.com>
2023-01-16 19:02:26 +0000

    alter the data

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,5 +1,5 @@
 with source as (
-    select * from {{ ref('raw_customers') }}
+    select * from {{ source('jaffle_shop', 'raw_customers') }}
 ),
 
 renamed as (
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,5 +1,5 @@
 with source as (
-    select * from {{ ref('raw_orders') }}
+    select * from {{ source('jaffle_shop', 'raw_orders') }}
 ),
 
 renamed as (
